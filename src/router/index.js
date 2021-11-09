053e69412d1e7cdefd41b0af7554b0386fdb6d25
jx <18965183850@163.com>
2021-11-09 10:35:44 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -6,59 +6,12 @@ Vue.use(Router)
 /* Layout */
 import Layout from '@/layout'
 
-export const constantRoutes = [{
+export const constantRoutes = [
+  {
     path: '/404',
     component: () => import('@/views/404'),
     hidden: true
   },
-  {
-    path: '',
-    redirect: '/build',
-    hidden: true
-  },
-  {
-    path: '/index',
-    component: () => import('@/views/index'),
-    hidden: true
-  },
-  {
-    path: '/first',
-    component: () => import('@/views/first/index')
-  },
-  {
-    path: '/htmls',
-    component: () => import('@/views/first/htmls')
-  },
-  {
-    path: '/build',
-    name: 'build',
-    component: () => import('@/views/build/index')
-  },
-  {
-    path: '/campus',
-    name: 'campus',
-    component: () => import('@/views/build/campusManagement')
-  },
-  {
-    path: '/teachers',
-    name: 'teachers',
-    component: () => import('@/views/build/teachers')
-  },
-  {
-    path: '/studentDevelopment',
-    name: 'studentDevelopment',
-    component: () => import('@/views/build/studentDevelopment')
-  },
-  {
-    path: '/networkTeaching',
-    name: 'networkTeaching',
-    component: () => import('@/views/build/networkTeaching')
-  },
-  {
-    path: '/OAProcess',
-    name: 'OAProcess',
-    component: () => import('@/views/build/OAProcess')
-  }
 ];
 
 export const notFoundRouter = [
